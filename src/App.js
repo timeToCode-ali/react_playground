9ff12f92dcc009f5c37c350cc0b41f78a4c1ade2
Alicia Rodriguez <aliciarodriguez@Alicias-MacBook-Pro.local>
2023-02-04 20:15:16 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>  Time to Code <span className='small'> with</span></h2> 
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+ 
+      </main>
     </div>
   );
 }
