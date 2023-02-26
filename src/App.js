e82aa286bc70c08cb5be729985823e30bcf2cef5
timeToCode-ali <time2code.ali@gmail.com>
2023-02-26 19:18:04 +0000

    countdown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import WordScramble from "./components/word-scramble/word-scramble";
+import Countdown from "./components/countdown/countdown";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          Time to Code <span className="small"> with</span>
-        </h2>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main>
-        <WordScramble />
-      </main>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h2>
+					Time to Code <span className='small'> with</span>
+				</h2>
+				<img src={logo} className='App-logo' alt='logo' />
+			</header>
+			<main>
+				<WordScramble />
+				<Countdown />
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/countdown/countdown.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const COUNTDOWN_TARGET = new Date("2023-03-31T23:59:59");
+
+const getTimeLeft = () => {
+	const totalTimeLeft = COUNTDOWN_TARGET - new Date();
+	const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
+	const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
+	const seconds = Math.floor((totalTimeLeft / 1000) % 60);
+	return { days, hours, minutes, seconds };
+};
+
+const Countdown = () => {
+	const [timeLeft, setTimeLeft] = useState(() => getTimeLeft());
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTimeLeft(getTimeLeft());
+		}, 1000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
+	return (
+		<div className='countdown'>
+			<h2>Countdown</h2>
+			<div className='content'>
+				{Object.entries(timeLeft).map((el) => {
+					const label = el[0];
+					const value = el[1];
+					return (
+						<div className='box' key={label}>
+							<div className='value'>
+								<span>{value}</span>
+							</div>
+							<span className='label'> {label} </span>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Countdown;
